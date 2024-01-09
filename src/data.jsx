f09a68f86c165f11c5547920edcbc1b07610a729
Pranav Sahu <pranavsahu28@gmail.com>
2024-01-09 14:27:27 +0530

    first react cart demo

---
+++ src/data.jsx
@@ -0,0 +1,43 @@
+//list of products presenting in the shop
+
+
+const data = {products:[{
+    id:"1",
+    name :"Head-set",
+    price:1500,
+    image: "https://rukminim2.flixcart.com/image/832/832/xif0q/headphone/y/x/y/-original-imagz2d8fkkf5vme.jpeg?q=70",
+},
+{
+    id:"2",
+    name :"Clock",
+    price:1200,
+    image:"https://rukminim2.flixcart.com/image/832/832/k44hksw0/wall-clock/3/z/n/403-403white-analog-kadio-original-imafn3chkfwasuqe.jpeg?q=70",
+},
+{
+    id:"3",
+    name :"George",
+    price:1000,
+    image:"https://cdn15.wildcraft.com/web-images/large/styles/QQXJDRLIASA/1580099454701/1.JPG",
+},
+{
+    id:"4",
+    name :"Head-set",
+    price:1500,
+    image:"https://tse1.mm.bing.net/th?id=OIP.zFuhX2Ykb9yoH4aPEX4sZwHaHa&pid=Api&P=0&w=300&h=300",
+},
+{
+    id:"5",
+    name :"Car",
+    price:4000000,
+    image:"https://d3jvxfsgjxj1vz.cloudfront.net/news/wp-content/uploads/2020/06/07103643/Toyota-Fortuner-19.jpg",
+},
+{
+    id:"6",
+    name :"Bat",
+    price:1000,
+    image:"https://assets.ajio.com/medias/sys_master/root/20210719/0MHH/60f5a333aeb269a9e347467b/-473Wx593H-4923366570-multi-MODEL.jpg",
+},
+
+],
+};
+export default data;
